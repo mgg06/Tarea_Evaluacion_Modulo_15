772e9571fd4fde7c8778d2b802b434f9fa983890
mgg0098@alu.medac.es <maartaagoonzalez12@gmail.com>
2026-04-07 13:13:54 +0200

    first commit

---
+++ test-vscode/src/test/java/com/entornos/CalculadoraRiesgoTest.java
@@ -0,0 +1,31 @@
+package com.entornos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraRiesgoTest {
+
+    CalculadoraRiesgo calc = new CalculadoraRiesgo();
+
+    @Test
+    void testEdadNegativa() {
+        assertEquals("Error", calc.evaluarEdad(-5));
+    }
+
+    @Test
+    void testAdulto() {
+        assertEquals("Adulto", calc.evaluarEdad(25));
+    }
+
+    @Test
+    void testSenior() {
+        assertEquals("Senior", calc.evaluarEdad(70));
+    }
+
+    @Test
+    void testLimite18() {
+        
+        assertEquals("Adulto", calc.evaluarEdad(18));
+    }
+
+}
